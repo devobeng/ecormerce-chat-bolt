cbc91ef5f25f029838f4c10a67266dfe52b3cc8c
dev.obeng.bismark@gmail.com <dev.obeng.bismark@gmail.com>
2024-12-16 08:52:53 +0000

    bag of words function, tokenize function stem function and setup traing data

--- bolt/nltk_util.py
+++ bolt/nltk_util.py
@@ -1,8 +1,15 @@
 import nltk
+nltk.download('punkt_tab')
+nltk.download('punkt')
+from nltk.stem.porter import PorterStemmer
+
+stemer= PorterStemmer()
 
 def tokenize(sentence):
-    pass
+    return nltk.word_tokenize(sentence)
 def stem(word):
-    pass
+    return stemer.stem(word.lower())
 def bad_of_words(tokenize_sentence, all_words):
     pass
+
+
---
+++ bolt/train.py
@@ -0,0 +1,32 @@
+import json
+from nltk_util import tokenize,stem,bad_of_words
+with open('intents.json') as file:
+    intents = json.load(file)
+    
+all_words=[]
+tags=[]
+xy=[]
+
+for intent in intents['intents']:
+    tag=intent['tag']
+    tags.append(tag)    
+    for pattern in intent['patterns']:
+        w=tokenize(pattern)
+        all_words.extend(w)
+        xy.append((w,tag))
+        
+ignore_words=['?','!','.',',']
+all_words=[stem(w) for w in all_words if w not in ignore_words]
+print(all_words)
+all_words=sorted(set(all_words))
+tags=sorted(set(tags))
+
+x_train=[]
+y_train=[]
+
+for (pattern_sentence,tag) in xy:
+    bag=bad_of_words(pattern_sentence,all_words)
+    x_train.append(bag)
+    
+    label=tags.index(tag)
+    y_train.append(label)
