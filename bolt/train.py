9089f0588b5bf380d4368c298af126c43cf2c46f
dev.obeng.bismark@gmail.com <dev.obeng.bismark@gmail.com>
2024-12-16 09:49:50 +0000

    build the model with pytorch

--- bolt/train.py
+++ bolt/train.py
@@ -1,6 +1,14 @@
 import json
 import numpy as np
 from nltk_util import tokenize,stem,bag_of_words
+from model import NeuralNet
+
+import torch 
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+
+
+
 with open('intents.json') as file:
     intents = json.load(file)
     
@@ -33,3 +41,55 @@ for (pattern_sentence,tag) in xy:
     y_train.append(label)
 x_train=np.array(x_train)
 y_train=np.array(y_train)
+class ChatDataset(Dataset):
+    def __init__(self):
+        self.n_samples=len(x_train)
+        self.x_data=x_train
+        self.y_data=y_train
+    def __getitem__(self,index):
+        return self.x_data[index],self.y_data[index]
+    def __len__(self):
+        return self.n_samples
+    
+    
+batch_size=8   
+hidden_size=8
+outputs_size=len(tags)
+input_size=len(x_train[0])
+learning_rate=0.001
+num_epochs=1000
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+dataset=ChatDataset()
+train_loader=DataLoader(dataset=dataset,batch_size=8,shuffle=True,num_workers=0)
+
+model = NeuralNet(input_size, hidden_size, outputs_size).to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+for epoch in range(num_epochs):
+    for (words, labels) in train_loader:
+        words = words.to(device)
+        labels = labels.to(dtype=torch.long).to(device)
+        
+        outputs = model(words)
+        loss = criterion(outputs, labels)
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+    if (epoch+1) % 100 == 0:
+        print (f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+
+print(f'final loss, loss: {loss.item():.4f}')
+
+data = {
+"model_state": model.state_dict(),
+"input_size": input_size,
+"hidden_size": hidden_size,
+"output_size": outputs_size,
+"all_words": all_words,
+"tags": tags
+}
